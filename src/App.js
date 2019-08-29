e7f9ce3897c4319398f7ff6f1cd1dbc4fb1f32ae
ph0ph0 <phillipalanlee@googlemail.com>
2019-08-29 12:04:20 +0100

    ref a DOM element

--- src/App.js
+++ src/App.js
@@ -323,22 +323,34 @@ Button.propTypes = {
   children: PropTypes.node
 }
 
-function Search( { value, onChange, onSubmit, children } ) {
+class Search extends Component {
 
-    return (
-      <form onSubmit = {onSubmit} > 
-        <input 
-        type = "text"
-        value = {value}
-        onChange = {onChange}
-        />
-        <button 
-          type = "submit"
-        >
-          {children}
-        </button>
-      </form>
-    )
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus()
+    }
+  }
+
+  render() {
+
+  const { value, onChange, onSubmit, children } = this.props
+
+  return (
+    <form onSubmit = {onSubmit} > 
+      <input 
+      type = "text"
+      value = {value}
+      onChange = {onChange}
+      ref = {el => this.input = el} 
+      />
+      <button 
+        type = "submit"
+      >
+        {children}
+      </button>
+    </form>
+  )
+  }
 }
 
 Search.propTypes = {
