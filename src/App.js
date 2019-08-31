bc88c4c18c03c5ac0c8ff5fa38f0a2423ea7a4a2
ph0ph0 <phillipalanlee@googlemail.com>
2019-08-31 11:25:18 +0100

    Passing function to setState()

--- src/App.js
+++ src/App.js
@@ -20,6 +20,32 @@ const SORTS = {
   POINTS: list => sortBy(list, 'points').reverse()
 }
 
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+  
+  const { searchKey, results} = prevState
+
+    console.log(`Setting search top stories`)
+
+    // Consider `const pageNumber = (results && results.page) || 0`. This means, if results and results.page is not null (or falsey),
+    // then use the results.page value (the latter of the two), if either are null, use 0. Hence, the below expression means if
+    // results and results[searchKey] is not null, use the hits stored under this key. If they are null, use an empty array. 
+    // && statements have precendence over other operators, hence bracketing the && statement or not makes no difference. 
+    const oldHits = (results && results[searchKey]) ? results[searchKey].hits : []
+
+    const updatedHits = [...oldHits, ...hits]
+
+    console.log(`Number of hits when setting top stories state: ${updatedHits.length}, pageNo.: ${page}`)
+
+    return (
+      {
+        results: {
+          ...results, [searchKey] : {hits: updatedHits, page}
+        },
+        isLoading: false
+      }
+    )
+}
+
 class App extends Component {
 
   _isMounted = false
@@ -56,28 +82,7 @@ class App extends Component {
 
     const { hits, page } = json
 
-    const { searchKey, results} = this.state
-
-    console.log(`Setting search top stories`)
-
-    // Consider `const pageNumber = (results && results.page) || 0`. This means, if results and results.page is not null (or falsey),
-    // then use the results.page value (the latter of the two), if either are null, use 0. Hence, the below expression means if
-    // results and results[searchKey] is not null, use the hits stored under this key. If they are null, use an empty array. 
-    // && statements have precendence over other operators, hence bracketing the && statement or not makes no difference. 
-    const oldHits = (results && results[searchKey]) ? results[searchKey].hits : []
-
-    const updatedHits = [...oldHits, ...hits]
-
-    console.log(`Number of hits when setting top stories state: ${updatedHits.length}, pageNo.: ${page}`)
-
-    this.setState(
-      {
-        results: {
-          ...results, [searchKey] : {hits: updatedHits, page}
-        },
-        isLoading: false
-      }
-    )
+    this.setState(updateSearchTopStoriesState(hits, page))
   }
 
   onSearchChange = e => {
